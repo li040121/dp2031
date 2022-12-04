4ba709097e20cc54d0d31f2e7e69152b39fa4d57
li040121 <81374397+li040121@users.noreply.github.com>
2022-12-03 20:48:12 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,15 +27,3 @@ resource "azurerm_resource_group" "example" {
   name     = "examplerg"
   location = "centralus"
 }
-
-resource "azurerm_storage_account" "examplesa" {
-  name                     = "ketanexamplesa"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    environment = "staging"
-  }
-}
